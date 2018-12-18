f922907a0e1cd22d6f24affa354d2d78ec2fa50c
Andrew Manzanero <manzanero.andrew@gmail.com>
2018-12-18 13:55:59 -0800

    1) Add nav bar and nav tests 2) Add procfile 3) update readme 4) Package updates to use "serve"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,35 +7,95 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Grow from '@material-ui/core/Grow';
+import Popper from '@material-ui/core/Popper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import MenuList from '@material-ui/core/MenuList';
+import Paper from '@material-ui/core/Paper';
+
 
 const styles = {
   root: {
     flexGrow: 1,
+    background: 'red',
   },
   grow: {
     flexGrow: 1,
+    align: 'center',
   },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
+    color: 'white',
+
   },
 };
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-      <AppBar>
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            Load the Barbell
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-  );
+class ButtonAppBar extends React.Component {
+
+    state = {
+        anchorEl: undefined,
+        open: false,
+    };
+    
+    handleToggle = () => {
+        this.setState(state => ({ open: !state.open }));
+    };
+    
+    handleClose = event => {
+        if (this.anchorEl.contains(event.target)) {
+            return;
+        }
+    
+        this.setState({ open: false });
+    };
+    
+    render() {
+        const { classes } = this.props;
+        const { open } = this.state;
+        return (
+            <AppBar className={classes.root}>
+                <Toolbar>
+                <IconButton 
+                className={classes.menuButton} 
+                aria-label="Menu"
+                buttonRef={node => {
+                    this.anchorEl = node;
+                  }}
+                  aria-owns={open ? 'menu-list-grow' : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleToggle}
+                  >
+                    <MenuIcon />
+                </IconButton>
+                <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+                    {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        id="menu-list-grow"
+                        style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                    >
+                        <Paper>
+                        <ClickAwayListener onClickAway={this.handleClose}>
+                            <MenuList>
+                            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                            <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                            </MenuList>
+                        </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                    )}
+                </Popper>
+                <Typography variant="h5" color="inherit" className={classes.grow}>
+                    Load the Barbell
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        );
+    }
 }
 
 ButtonAppBar.propTypes = {
