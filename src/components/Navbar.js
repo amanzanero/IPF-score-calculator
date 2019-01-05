50f91ae58ab574816eb31061bae0e10830fe68bc
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-01-05 12:37:38 -0800

    added file description for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,7 @@
+/*
+Nav bar contains title with menu
+- need to add login functionality
+*/
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
