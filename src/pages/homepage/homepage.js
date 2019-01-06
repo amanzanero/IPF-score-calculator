88be6099f4b58b2ff5594f1025fe343e901b75cf
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-01-05 16:04:43 -0800

    added radio buttons for gender and event type

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -11,17 +11,44 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 const UNITS = ['kilos', 'pounds'];
-const GENDER = ['men', 'women'];
+const GENDER = [
+  {
+    label: 'Man',
+    value: 'men',
+  },
+  {
+    label: 'Woman',
+    value: 'women'
+  },
+];
+const EVENTS = [
+  {
+    label:  'Classic 3-Lift',
+    value: 'classicThreeLift'
+  },
+  {
+    label: 'Classic Bench',
+    value: 'classicBench',
+  },
+  {
+    label: 'Equipped 3-Lift',
+    value: 'equippedThreeLift',
+  },
+  {
+    label: 'Equipped Bench',
+    value: 'equippedBench',
+  },
+]
 const CONSTANTS = {
   'men': {
     'classicThreeLift': [310.67,	857.785,	53.216,	147.0835],
-    'clasicBench': [86.4745, 259.155, 17.57845, 53.122],
+    'classicBench': [86.4745, 259.155, 17.57845, 53.122],
     'equippedThreeLift': [387.265, 1121.28, 80.6324, 222.4896],
     'equippedBench': [133.94, 441.465, 35.3938, 113.0057],
   },
   'women': {
     'classicThreeLift': [125.1435, 228.03, 34.5246, 86.8301],
-    'clasicBench': [25.0485, 43.848, 6.7172, 13.952],
+    'classicBench': [25.0485, 43.848, 6.7172, 13.952],
     'equippedThreeLift': [176.58, 373.315, 48.4534, 110.0103],
     'equippedBench': [49.106, 124.209, 23.199, 67.4926],
   },
@@ -31,12 +58,13 @@ const styles = (theme) => ({
   root: {
     display: 'flex',
   },
-  // formControl: {
-  //   margin: theme.spacing.unit * 3,
-  // },
-  // group: {
-  //   margin: `${theme.spacing.unit}px 0`,
-  // },
+  formControl: {
+    marginTop: '10%',
+    margin: '0 auto',
+  },
+  group: {
+    margin: 'auto',
+  },
 });
 
 class HomePage extends React.PureComponent {
@@ -45,15 +73,19 @@ class HomePage extends React.PureComponent {
   */
   
   state = { 
-    gender: GENDER[0],
-    constantSet: CONSTANTS[GENDER[0]].classicThreeLift,
-    bodyWeight: null,
+    gender: 'men',
+    constantSet: CONSTANTS.men.classicThreeLift,
+    eventType: 'classicThreeLift',
+    bodyWeight: 64,
     total: 0,
     units: UNITS[0],
+    ipfScore: 0
   }
 
   handleChange = (prop) => (event) => {
     this.setState({ [prop]: event.target.value });
+    this.setState({ 'constantSet': CONSTANTS[this.state.gender][this.state.eventType] })
+    console.log(this.state);
   };
 
   calculateScore = (total, bodyWeight, constants) => {
@@ -67,19 +99,37 @@ class HomePage extends React.PureComponent {
 
     return (
       <div className={classes.root}>
+
         <FormControl component="fieldset" className={classes.formControl}>
           <FormLabel component="legend">Gender</FormLabel>
           <RadioGroup
             aria-label="Gender"
             name="gender1"
             className={classes.group}
-            value={this.gender}
+            value={this.state.gender}
             onChange={this.handleChange('gender')}
           >
-            <FormControlLabel value={GENDER[0]} control={<Radio />} label={GENDER[0]} />
-            <FormControlLabel value={GENDER[1]} control={<Radio />} label={GENDER[1]} />
+            {GENDER.map((option) => (
+              <FormControlLabel value={option.value} control={<Radio />} label={option.label} />
+            ))}
           </RadioGroup>
         </FormControl>
+
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Lift</FormLabel>
+          <RadioGroup
+            aria-label="Lift"
+            name="lift1"
+            className={classes.group}
+            value={this.state.eventType}
+            onChange={this.handleChange('eventType')}
+          >
+            {EVENTS.map((option) => (
+              <FormControlLabel value={option.value} control={<Radio />} label={option.label} />
+            ))}
+          </RadioGroup>
+        </FormControl>
+
       </div>
     )
   }
