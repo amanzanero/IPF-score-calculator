036951dd997bd2091115da435fbed61a00d8f97f
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-04-15 10:17:55 -0700

    merge from master & add wilks

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,13 +5,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
+import classNames from 'classnames';
+import { Paper, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import Grid from '@material-ui/core/Grid';
+
 import RadioButtonField from './radiobuttonfield';
 import WeightEntry from './weightentry';
 import { CONSTANTS } from '../../constants/constants';
-import { Paper, Typography } from '@material-ui/core';
-import classNames from 'classnames';
-import { loadCSS } from 'fg-loadcss/src/loadCSS';
-import Icon from '@material-ui/core/Icon';
+import { WILKS_CONSTANTS } from "../../constants/constants";
+
 
 
 const styles = (theme) => ({
@@ -24,7 +28,8 @@ const styles = (theme) => ({
     display: 'flex',
     margin: '0 auto',
     flexWrap: 'wrap',
-    maxWidth: '400px'
+    maxWidth: '400px',
+    marginBottom: '1em'
   },
   group: {
     margin: 'auto',
@@ -33,7 +38,11 @@ const styles = (theme) => ({
     marginTop: '.75em',
   },
   logo: {
-    marginTop: '4em'
+    marginTop: '2.5em',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '.5em',
+    width: '75px'
   },
   width: {
     width: '100%',
@@ -42,8 +51,8 @@ const styles = (theme) => ({
     marginLeft: '1.5em',
     marginRight: '1.5em',
   },
-  bottomMargin: {
-    marginBottom: '3em'
+  ico: {
+    transform: 'scale(1.75)',
   }
 });
 
@@ -60,17 +69,10 @@ class HomePage extends React.Component {
       bodyWeight: '',
       total: '',
       units: 'kilos',
-      ipfScore: '0.0'
+      ipfScore: '0.0',
+      wilksScore: '0.0',
     }
   };
-
-  componentDidMount(){
-    loadCSS(
-      'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
-      document.querySelector('#insertion-point-jss'),
-    );
-  }
-
 
   handleChange = (event) => {
     const target = event.target;
@@ -91,30 +93,62 @@ class HomePage extends React.Component {
     var weight = Number(this.state.bodyWeight);
     var total = Number(this.state.total);
 
+    /* Check inputs to make sure they are valid */
     if (isNaN(weight) || isNaN(total)) return;
 
+    /* Both calculations need to be done in kg */
     if (this.state.units === 'pounds') {
       weight = this.state.bodyWeight * 0.453592;
       total = this.state.total * 0.453592;
     };
+
+    /**
+     * IPf calculation
+     */
     var constants = CONSTANTS[this.state.gender][this.state.eventType][this.state.liftType];
     var score = 500 + 100 *( total - ( constants[0] * Math.log(weight) - constants[1] ))
     / ( constants[2] * Math.log(weight) - constants[3] );
-    console.log(score)
+
+    /**
+     * Wilks calculation
+     */
+    var C = WILKS_CONSTANTS[this.state.gender];
+    var a = C['a'];
+    var b = C['b'] * weight;
+    var c = C['c'] * Math.pow(weight,2);
+    var d = C['d'] * Math.pow(weight,3);
+    var e = C['e'] * Math.pow(weight,4);
+    var f = C['f'] * Math.pow(weight,5);
+
+    var wilksCoeff = 500 / (a+b+c+d+e+f);
+    var wScore = wilksCoeff * total;
+
+    /* So we don't output anything that doesnt fit */
     score = Math.round(score*100)/100;
+    wScore = Math.round(wScore*100)/100;
     let stringScore;
-    if (score > 99999999){
-      stringScore = "Stop lying ;)";
+    let wStringScore
+    if (score > 999){
+      stringScore = "u sure?";
     }
     else if (isNaN(score) || score < 0){
-      return;
-    }
-    else {
+      stringScore = "0.0";
+    } else {
       stringScore = score.toString();
     }
-    console.log(stringScore)
+
+    if (wScore > 999) {
+      wStringScore = "u sure?";
+    }
+    else if (isNaN(wScore) || wScore < 0){
+      wStringScore = "0.0";
+    } else {
+      wStringScore = wScore.toString();
+    }
+
     this.setState({
       ipfScore: stringScore,
+      wilksScore: wStringScore
     })
   };
 
@@ -127,12 +161,24 @@ class HomePage extends React.Component {
 
         <Paper
         className={classNames(classes.control, classes.sideMargin, classes.width)}>
-          <Typography align="center" variant="h6" color= "secondary">
-            {"Score:"}
-          </Typography>
-          <Typography align="center" variant="h3" color= "primary">
-            {this.state.ipfScore}
-          </Typography>
+         <Grid container spacing="24">
+          <Grid item xs={6}>
+            <Typography align="center" variant="h6" color="secondary">
+              {"IPF:"}
+            </Typography>
+            <Typography align="center" variant="h4" color="primary">
+              {this.state.ipfScore}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography align="center" variant="h6" color="secondary">
+              {"Wilks:"}
+            </Typography>
+            <Typography align="center" variant="h4" color="primary">
+              {this.state.wilksScore}
+            </Typography>
+          </Grid>
+          </Grid>
         </Paper>
 
         <WeightEntry
@@ -151,22 +197,26 @@ class HomePage extends React.Component {
         size="large"
         color="primary"
         className={classNames(classes.button, classes.sideMargin, classes.width)}
-        onClick={this.calculateScore} >
+        onClick={()=>{
+          this.calculateScore();
+          }} >
           Calculate
         </Button>
 
         <Button variant="outlined"
         size="large"
         color="secondary"
-        className={classNames(classes.logo, classes.sideMargin)}
-        fullWidth
+        className={classes.logo}
         onClick={() => {window.open('https://github.com/amanzanero/IPF-score-calculator')}} >
-          <Icon className={classNames(classes.icon, 'fab fa-github')} color="primary" />
+          <FontAwesomeIcon
+          icon={faGithub}
+          className={classes.ico}
+          />
         </Button>
 
         <Typography
         align="center"
-        className={classNames(classes.width, classes.sideMargin, classes.bottomMargin)}>
+        className={classNames(classes.width, classes.sideMargin)}>
           Not afiliated with the IPF.
         </Typography>
       </div>
