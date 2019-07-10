9a5022c3e468a88fe34a66348da9b9816ca67826
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-07-10 17:35:47 -0400

    Updates

---
+++ precache-manifest.0a980e16eda824d7b810f9c126da6ac7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b97ab0acfd00549d768",
+    "url": "/ipf-score-calculator/static/js/runtime~main.3c652f72.js"
+  },
+  {
+    "revision": "4cc8ce18af534a1066e3",
+    "url": "/ipf-score-calculator/static/js/main.73478d52.chunk.js"
+  },
+  {
+    "revision": "1e6f1c21ba9f88a1270c",
+    "url": "/ipf-score-calculator/static/js/2.04365cad.chunk.js"
+  },
+  {
+    "revision": "74329a8b6f78ce263d4c39a1eaac656c",
+    "url": "/ipf-score-calculator/index.html"
+  }
+];
